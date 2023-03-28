60f7bd5771e131571aa649c1a667ae3a0902bbf7
de0r <oshanhamal3@gmail.com>
2023-03-28 10:47:04 +0545

    Genric fetch data hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,20 +5,20 @@ import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error, loading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = "1,2,3,4,5,6,7,8,9,10,11,12".split(",");
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 4 }} padding="10px" spacing={10}>
-        {loading &&
+        {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
