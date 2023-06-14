46de44e88f87eb6f0f69f35d5838569a0823f50c
de0r <oshanhamal3@gmail.com>
2023-06-14 13:14:55 +0545

    Refactor: Entity Interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import platforms from "../data/platforms";
+import Platform from "../entities/Platform";
 import APIClient from "../services/api-client";
-import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
